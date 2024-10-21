c8bacc6d6c1c5e196ef19c18e5af6098976493e2
Reece He <reesehe.66146@gmail.com>
2024-10-21 17:50:42 +0100

    Refactored the code to change variable conventions so that they match, edited the font gen and added a button gen, added some text and button sprites and a loading screen.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # --- This is a pygame fighting game ---
-import sys
+import time
 import pygame as pg
-import random as r
 pg.init()
+from pygame.locals import *
 if not pg.get_init():
     print("There was something wrong with the program *>*")
-    sys.exit()
+    quit()
 # import math
+# import random as r
 
+"""
 # --- Sprite Class ---
 # Creates and assigns attributes to the sprite
 class Spr:
@@ -70,103 +72,132 @@ class Spr:
             pg.event.set_allowed([pg.KEYDOWN, pg.MOUSEBUTTONUP])
             print(self.name, self.health)
 
-    """
     def ranGen(self, mini, maxi):
         randomNumber = r.randint(int(r.random() * mini), int(r.random() * maxi))
         return randomNumber
-    """
+"""
+
 # --- Font generator ---
 """
 Font class takes various arguments to create an instance of text with its attributes
 This makes it easier to place fonts during runtime as only the location will change
  - Font and size creates the type of font that will be used with the text
  - Text, Antialias, Text Colour and Background Colour are attributes of the text and its the text box
- - Text is moved with a tuple which contains its new position     
 """
-class FontGEN:
-    def __init__(self, font, size, text, antialias, textColour, backgroundColour):#, textBackground, textTransformation):
+# --- Text generator ---
+class TextGen:
+    def __init__(self, font, size, text, antialias, textColour, backgroundColour, centrePosX, centrePosY):
         self.font = pg.font.Font(font, size)
         self.text = self.font.render(text, antialias, textColour, backgroundColour)
         self.rect = self.text.get_rect()
-        self.rect.center = (WinSize[0]//2, WinSize[1]//2)
+        self.rect.center = (centrePosX, centrePosY)
+        self.originX = self.rect[0]
+        self.originY = self.rect[1]
+        self.endX = self.originX + self.rect[2]
+        self.endY = self.originY + self.rect[3]
 
     # Renders the text onto the screen
     def render(self):
         Window.blit(self.text, self.rect)
 
-    # Changes the position of the textbox's centre with coordinates in tuple
-    def move(self, newPosition):
-        self.rect.center(newPosition[0], newPosition[1])
+# --- Button generator ---
+# Works similarly to the font generator but has interactivity by checking whether there is input within its hit box
+class ButtonGen:
+    def __init__(self, font, size, text, antialias, textColour, backgroundColour, centrePosX, centrePosY):
+        self.font = pg.font.Font(font, size)
+        self.text = self.font.render(text, antialias, textColour, backgroundColour)
+        self.rect = self.text.get_rect()
+        self.rect.center = (centrePosX, centrePosY)
+        self.originX = self.rect[0]
+        self.originY = self.rect[1]
+        self.endX = self.originX + self.rect[2]
+        self.endY = self.originY + self.rect[3]
+
+    # Renders the button onto the screen
+    def render(self):
         Window.blit(self.text, self.rect)
 
-
-"""
-# --- Button generator ---
-# Works similarly to the font generator but has interactivity
-class ButtonGEN:
-    def __init__(self, type, rect, position):
-        if type == "Image":
-            self.rect = pg.rect.rect()
-        elif type == "Drawn":
-            self.rect = pg.rect.rect()
-            pg.draw.rect(Window, [000, 000, 000], self.button)
-"""
+    # Checks whether the input is within the button parameters
+    def positionCheck(self, MousePosition):
+        if (self.originX < MousePosition[0] < self.endX) and (self.originY < MousePosition[1] < self.endY):
+            return True
 
 
 # --- Main ---
 # Loads the variables and constants
-WinSize = (900, 450)
+WinSize = (480, 270)
 Clock = pg.time.Clock()
-stateInMenu = False
-stateInOptions = False
-stateInCharacterSelection = False
-stateInMapSelection = False
-stateInGame = False
+#Preset colours which will be used in the menus
+BackgroundColour = (66, 129, 164)
+AccentColour = (72, 169, 166)
+SubBackgroundColour = (228, 223, 218)
+SubAccentColour = (212, 180, 131)
+#Game states used to determine what part of teh code can be executed
+StateInMenu = False
+StateInOptions = False
+StateInCharacterSelection = False
+StateInMapSelection = False
+StateInGame = False
 
 # Load assets - Loads assets such as images and fonts
-defaultFont = "Other assets\Agdasima\Agdasima-Regular.ttf"
-alternativeFont = "freesansbold.ttf"
-icon = pg.image.load("Game assets\Icon.png")
-char1Still = pg.image.load("Game assets\Char1Still.png")
-#char2Still = pg.image.load("Game assets\")
-#char3Still = pg.image.load("Game assets\")
-
-
-
-# Sprite creation - Creates all sprites to be used in the game.
-p1 = Spr("player 1!!!", [300, 150, 20, 55], char1Still)
-p2 = Spr("player 2!!!", [300, 150, 20, 55], char1Still)
+DefaultFont = "Other assets\Agdasima\Agdasima-Regular.ttf"
+AlternativeFont = "freesansbold.ttf"
+TempBG = "Other assets\Archive\SizeTest3.png"              ### Remove this
+TempChar = "Other assets\Archive\TempChar.png"             ### Remove this
+Icon = pg.image.load("Game assets\Icon.png")
+Char1Still = pg.image.load("Game assets\Char1Still.png")
+#Char2Still = pg.image.load("Game assets\")                ### Create image
+#Char3Still = pg.image.load("Game assets\")
+Background1 = pg.image.load("Game assets\BG1_Ring.png")
+Background2 = pg.image.load("Game assets\BG2_Beach.png")
+Background3 = pg.image.load("Game assets\BG3_Woodlands.png")
+#Background4 = pg.image.load("Game assets\BG4_Hell.png)    ### Create image
 
 # Loads the screen and visual assets onto the screen
-pg.display.set_icon(icon)###
-pg.display.set_caption("Draft - PyG Battle program")
+pg.display.set_icon(Icon)
+pg.display.set_caption("PyG Battle program")
 Window = pg.display.set_mode(WinSize)
 
-Window.blit(char1Still, (0, 175))#pg.draw.rect(Window, p2.colour, p2.rect)
-Window.blit(char1Still, (0, 175))#pg.draw.rect(Window, p2.colour, p2.rect)
-font1 = FontGEN(defaultFont, 45, "This finally worked", False, (0, 0, 0), (155, 000, 000))#"Other assets\Agdasima\Agdasima-Regular.ttf"
-font1.render()
+#Loads all the text and buttons to be used in the game
+#(font, size, text, antialias, textColour, backgroundColour, centrePosX, centrePosY)
+LoadingText = TextGen(DefaultFont, 35, "Loading The Game...Please Wait", True, SubAccentColour, SubBackgroundColour, WinSize[0] // 2, WinSize[1] // 2)
+WelcomeText = TextGen(DefaultFont, 30, "FightingGame Booted", True, SubAccentColour, SubBackgroundColour, WinSize[0] // 2, WinSize[1] // 2)
+"""
+StartGameButton = ButtonGen()
+SettingButton = ButtonGen()
+ReturnButton = ButtonGen()
+CharSelectText = TextGen()
+Player1Text = TextGen()
+Player2Text = TextGen()
+Char1Button = ButtonGen()
+Char2Button = ButtonGen()
+Char3Button = ButtonGen()
+Char4Button = ButtonGen()
+Background1Button = ButtonGen()
+Background2Button = ButtonGen()
+Background3Button = ButtonGen()
+Background4Button = ButtonGen()
+"""
+
+#A loading screen which waists the players' time since this is not needed and also yes, I spelt it waist not waste
+Window.fill(BackgroundColour, pg.Rect(0, 0, WinSize[0], WinSize[1]))
+LoadingText.render()
+pg.display.update()
+time.sleep(5)
+#The welcome screen which notifies the user the pointless loading screen has been completed
+Window.fill(BackgroundColour, pg.Rect(0, 0, WinSize[0], WinSize[1]))
+WelcomeText.render()
+pg.display.update()
+time.sleep(3)
+
 pg.display.update()
 # The main loop which runs the game
 while True:
     for event in pg.event.get():
-        if (event.type == pg.QUIT) or (event.type == pg.K_ESCAPE):
+        if (event.type == pg.QUIT) or ((event.type == pg.KEYDOWN) and (event.key == K_ESCAPE)):
             pg.quit()
             print("Exited pygame")
             quit()
-        elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_w:
-                p1.moveForward()
-                font1.render()
-        elif event.type == pg.KEYUP:
-            if event.type == pg.K_p:
-                p1.punch(p2)
-            elif event.type == pg.K_k:
-                p1.kick(p2)
-            """
-            elif event.key == pg.K_h:
-                p1.heal()
-            """
         # This is done every cycle to keep the game running smoothly
-        pg.display.update()
-        Clock.tick(60)
+    pg.display.update()
+    Clock.tick(60)
